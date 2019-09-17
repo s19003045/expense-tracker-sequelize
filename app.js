934b3f552c554c781e42c9247617b57796209fae
Gary Wu <s19003045@gmail.com>
2019-09-17 23:54:36 +0800

    Add BDD test to RESTful API:  GET/  &  POST/add

--- app.js
+++ app.js
@@ -2,17 +2,42 @@ const express = require('express')
 const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 
+
+// Set body-parser
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 // Set up template engine 
 app.engine('handlebars', exphbs())
 app.set('view engine', 'handlebars')
 
 
-app.get('/', (req, res, next) => {
-  console.log('This is home page')
-  res.render('index')
-})
+const router = express.Router();
+
+router.get('/', function (req, res) {
+  res.json({ "error": false, "message": "Hello !" });
+});
+
+router.post('/add', function (req, res) {
+  res.json({ "error": false, "message": "success", "data": req.body.num1 + req.body.num2 });
+});
+
+app.use('/', router);
+
+
+
+
+// app.get('/', (req, res) => {
+//   // console.log('This is home page')
+//   // res.render('index')
+//   res.json({ "error": false, "message": "Hello !" });
+// })
+
+// app.post('/', (req, res) => {
+//   res.json({ "error": false, "message": "success", "data": req.body.num1 + req.body.num2 })
+// })
 
 // Server start
 app.listen(port, () => {
