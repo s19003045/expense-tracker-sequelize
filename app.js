23e33485c57b581ae9d284df8e2b317fb901a351
Gary Wu <s19003045@gmail.com>
2019-09-17 19:52:46 +0800

    Set template engine and add layout plus partials

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const exphbs = require('express-handlebars')
+
+
+// Set up template engine 
+app.engine('handlebars', exphbs())
+app.set('view engine', 'handlebars')
+
 
 app.get('/', (req, res, next) => {
   console.log('This is home page')
-  res.send('Home page')
+  res.render('index')
 })
 
+// Server start
 app.listen(port, () => {
   console.log(`Express server start`)
 })
+
+
