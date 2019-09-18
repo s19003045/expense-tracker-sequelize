9718b0518a1b46a5f474a92819f438907ad0e435
Gary Wu <s19003045@gmail.com>
2019-09-18 23:57:15 +0800

    Add totalAmount method to record Schema, modify record.json and create calculate constructor

--- models/seeds/seeder.js
+++ models/seeds/seeder.js
@@ -5,21 +5,33 @@ const records = require('../record.json')
 mongoose.connect('mongodb://localhost/record', { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
 const db = mongoose.connection
 
-console.log(Record({ amount: 15 }).amount)
-
+const calculate = require('../calculate.js')
+const calculateForRecords = new calculate()
 
 db.on('error', () => {
   console.log('connect to mongoDB error')
 })
 
 db.once('open', () => {
-  // console.log(records.results)
+
+  // Add seeds to mongoDB
   records.results.forEach(element => {
     Record.create({
       name: element.name,
       category: element.category,
-      amount: element.amount
+      amount: element.amount,
+      unitPrice: element.unitPrice
     })
   })
+
+
+  // Record.find().sort({ name: 'asc', date: 'asc' }).then(records => {
+  //   let totalPrice = calculateForRecords.totalPrice(records)
+  //   let recordCount = calculateForRecords.recordCount(records)
+  //   let average = calculateForRecords.average(totalPrice, recordCount)
+
+  //   console.log('=====================')
+  //   console.log(`總花費： ${totalPrice}  總筆數：${recordCount}  平均：${average}`)
+  // })
 
 })
