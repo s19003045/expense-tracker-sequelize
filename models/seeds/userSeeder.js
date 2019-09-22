823487adc7646884a44c22b179f493c194393353
Gary Wu <s19003045@gmail.com>
2019-09-22 08:10:53 +0800

    Add user model and seeds for user

---
+++ models/seeds/userSeeder.js
@@ -0,0 +1,43 @@
+// import mongoose and set up connection to mongoDB
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/record', { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
+const db = mongoose.connection
+
+// User model
+const User = require('../user.js')
+
+// import userList as seeds
+const { users: userList } = require('../user.json')
+
+// import cryptjs
+const bcrypt = require('bcryptjs')
+
+db.on('error', () => {
+  console.log('connect to mongoDB error')
+})
+
+db.once('open', () => {
+
+  // Add seeds to mongoDB
+  userList.forEach(element => {
+    console.log(element)
+    bcrypt.genSalt(10, (err, salt) => {
+      if (err) return console.error(err)
+      bcrypt.hash(element.password, salt, (err, hash) => {
+        if (err) throw err
+        User
+          .create({
+            name: element.name,
+            email: element.email,
+            password: hash
+          })
+          .then(user => {
+            console.log(user)
+          })
+          .catch(err => { console.log(err) })
+      })
+    })
+
+  })
+
+})
